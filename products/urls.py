aa02e9d9cbbfa9a47f9f99dd2a7de66a0e282fd1
Himanshu Sagar <sagarhimanshu355@gmail.com>
2022-12-17 19:00:02 +0530

    updating requirements.txt

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.ProductCreateDeleteUpdateView.as_view(), name='product-create-update'),
     path('list', views.ProductListView.as_view(), name='product-list'),
     path('delete/<str:pk>', views.ProductCreateDeleteUpdateView.as_view(), name='product-delete'),
-    path('update_quantity/<str:pk>', views.ProductCreateDeleteUpdateView.as_view(), name='product-update-quantity')
+    path('update/<str:pk>', views.ProductCreateDeleteUpdateView.as_view(), name='product-update')
 ]
